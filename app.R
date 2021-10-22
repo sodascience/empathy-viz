1815c352a93b3bdc2209cc72df6ef8eb3a42bbaf
Shiva Nadi <s.nadi@uu.nl>
2021-10-22 14:29:49 +0200

    Layout Questionnair panel

--- app.R
+++ app.R
@@ -22,10 +22,70 @@ ui <- fluidPage(navbarPage(theme = bslib::bs_theme(bootswatch = "flatly"),
                        textInput("Gender", placeholder = "Gender", label = "Gender:", width = "30%"),
                        textInput("Age", placeholder = "Age", label = "Age:", width = "30%"),
                        textInput("Code", placeholder = "Code", label = "Code:", width = "30%"),
-                       submitButton("Submit")
+                       submitButton("Submit", icon = NULL, width = '100px')
                    )
                  )),
-                 tabPanel("Questionnair"),
+                 tabPanel(
+                   "Questionnair",
+                   mainPanel(
+                     verticalLayout(
+                       strong("Survay"),
+                       p(style="text-align: justify;",
+                        "We maken het allemaal wel eens mee dat we zien dat iemand zich pijn doet,
+                         verdrietig is of juist heel blij is. Als we zien dat iemand zich bijvoorbeeld snijdt,
+                         het hoofd stoot of struikelt op straat dan weten we dat dat pijn doet maar voelen dat
+                         soms ook. Als we horen dat iemand gepest is of buitengesloten wordt dan vinden we dat
+                         vaak zielig en soms worden we van iemand die heel blij is zelf ook een beetje blij.
+                         Het meeleven of meevoelen met de emoties van anderen doen we soms ongemerkt en met
+                         de ene persoon meer dan met de ander. Wat wij graag van jou willen weten is wat jij
+                         voelt en doet wanneer je ziet dat iemand verdrietig is, pijn heeft of juist blij is.",
+                         style = "font-family: 'times'; font-si18pt"),
+                       p(style="text-align: justify;",
+                        "de volgende bladzijde staan een aantal uitspraken die gaan over het meevoelen met
+                        anderen in verschillende situaties. Het kan natuurlijk zijn dat je nog nooit zo'n
+                        situatie hebt meegemaakt. Probeer je dan voor te stellen hoe dat zou zijn,
+                        wat je zou voelen en willen doen.",
+                        style = "font-family: 'times'; font-si18pt"
+                        ),
+                       p(style="text-align: justify;",
+                         "Er zijn geen goede of foute antwoorden. Het gaat om jouw eigen gevoel.",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "Je antwoord geef je door het cijfer te omcirkelen wat het meest op jou van toepassing is:",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "1= helemaal niet van toepassing",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "2= een beetje van toepassing",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "3= redelijk goed van toepassing",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "4= sterk van toepassing",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       p(style="text-align: justify;",
+                         "5= heel sterk van toepassing",
+                         style = "font-family: 'times'; font-si18pt"
+                         
+                       ),
+                       actionButton("click", "Start", width='100px')
+                     )
+                   )
+                   ),
                  tabPanel("Results")
 ))
 
