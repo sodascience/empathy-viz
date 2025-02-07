74c958d83b26cea9cb3b6ecf8d19d74f1473103d
parisa-zahedi <parisa.zahedi@gmail.com>
2025-02-07 18:09:51 +0100

    disable tabs if akkoord is unchecked (#25)
    Co-authored-by: parisa-zahedi <p.zahedi@uu.nl>

--- survey-app.R
+++ survey-app.R
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 library(shiny)
+library(shinyjs)
 source("mod-disclaimer.R")
 source("mod-question.R")
 source("mod-image.R")
@@ -26,101 +27,95 @@ source("mod-visRelationship.R")
 source("mod-visEmotions.R")
 source("mod-visRelEmo.R")
 
-ui <- navbarPage(theme = bslib::bs_theme(bootswatch = "flatly"),
-                           id = "main.navbar",
-                           # Application title
-                           "Empathie in Beeld",
-                            shinyjs::useShinyjs(),
-                 
-                           # Start Disclaimer-tab
-                           tabPanel(
-                             "Disclaimer",
-                             
-                             # Disclaimer-tab body
-                             wellPanel(
-                               #style= "min-width: 300px;max-width: 400px;overflow:auto",
-                               #verticalLayout(
-                                 disclaimerUI("dsc1")
-                               #)
-                             )
-                           ),
-                           # End Disclaimer-tab          
-                 
-                           # Start Input-tab
-                           tabPanel(
-                             title = "Input",
-                             value = "input_tab",
-                             
-                             # Input-tab body
-                             wellPanel(
-                               style= "min-width: 300px;max-width: 400px;overflow:auto",
-                               verticalLayout(
-                                 inputUI("inp1")
-                               )
-                              )
-                             ),
-                            # End Input-tab
-
-
-                           # Start Questionnaire-tab
-                           tabPanel(
-                             title = "Vragenlijst",
-                             value = "tvrag",
-
-                             # Progress bar
-                             sidebarLayout(
-                               sidebarPanel(
-                                 imageUI("img1"),
-                                 width = 3
-
-                               ),
-
-                               mainPanel(
-                                 questionUI("surv1") ,
-                                 width = 9
-                               )
-                             )
-                           ),
-                          # End Questionnaire-tab
-
-                           # Start Visualization-tab
-                         tabPanel(
-                           "Visualisatie",
-                           tabsetPanel(
-                             id = "vistab",
-                             tabPanel(title = "Selecteer Dataset",
-                                      value = "tdataset",
-                                      visDatasetUI("visDataset1")
-                             ),
-                             navbarMenu("Dynamiek in relaties",
-                                        tabPanel(title = "Blijdschap",
-                                                 value = "tblij",
-                                                 visRelationshipUI("visRelationship2")),
-                                        tabPanel(title = "Verdriet",
-                                                 value = "tverd",
-                                                 visRelationshipUI("visRelationship3")),
-                                        tabPanel(title = "Pijn",
-                                                 value = "tpijn",
-                                                 visRelationshipUI("visRelationship1"))
-
-                             ),
-                             tabPanel("Relaties * Emoties",
-                                      visRelEmoUI("visRelEmo1")),
-                             tabPanel(title = "Dynamiek in emoties",
-                                      value = "temo",
-                                      visEmotionUI("visEmotion1")
-                             )
-                           ))
-                          # End Visualization-tab
-
+ui <- navbarPage(
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  id = "main.navbar",
+  title = "Empathie in Beeld",
+  shinyjs::useShinyjs(),
+  
+  # Disclaimer-tab
+  tabPanel(
+    "Disclaimer",
+    wellPanel(
+      disclaimerUI("dsc1")
+    )
+  ),
+  
+  # Input-tab
+  tabPanel(
+    title = "Input",
+    value = "input_tab",
+    wellPanel(
+      style = "min-width: 300px;max-width: 400px;overflow:auto",
+      verticalLayout(
+        inputUI("inp1")
+      )
+    )
+  ),
+  
+  # Questionnaire-tab
+  tabPanel(
+    title = "Vragenlijst",
+    value = "tvrag",
+    sidebarLayout(
+      sidebarPanel(
+        imageUI("img1"),
+        width = 3
+      ),
+      mainPanel(
+        questionUI("surv1"),
+        width = 9
+      )
+    )
+  ),
+  
+  # Visualization-tab
+  tabPanel(
+    "Visualisatie",
+    tabsetPanel(
+      id = "vistab",
+      tabPanel(
+        title = "Selecteer Dataset",
+        value = "tdataset",
+        visDatasetUI("visDataset1")
+      ),
+      navbarMenu(
+        "Dynamiek in relaties",
+        tabPanel(
+          title = "Blijdschap",
+          value = "tblij",
+          visRelationshipUI("visRelationship2")
+        ),
+        tabPanel(
+          title = "Verdriet",
+          value = "tverd",
+          visRelationshipUI("visRelationship3")
+        ),
+        tabPanel(
+          title = "Pijn",
+          value = "tpijn",
+          visRelationshipUI("visRelationship1")
+        )
+      ),
+      tabPanel(
+        "Relaties * Emoties",
+        visRelEmoUI("visRelEmo1")
+      ),
+      tabPanel(
+        title = "Dynamiek in emoties",
+        value = "temo",
+        visEmotionUI("visEmotion1")
+      )
+    )
+  )
 )
+
 server <- function(input, output, session) {
   counter <- reactiveVal(0)
   input.data <- reactiveValues(gender = NULL, age = NULL, code = NULL)
   df.survey <- reactiveValues(data = NULL)
   df.vis <- reactiveValues(data = NULL)
   disclaimer_file_path = "data/disclaimer.csv"
-
   questionServer("surv1", "data/introduction.csv","data/vignettes.csv",
                  "data/relationships.csv", "data/RadioMatrixFrame.csv",
                  "data/ending.csv",counter,input.data,df.survey)
@@ -133,5 +128,15 @@ server <- function(input, output, session) {
   visRelationshipServer("visRelationship3",df.vis,"data/vignettes.csv","data/guideline_vis.csv",'verdriet')
   visEmotionServer("visEmotion1","data/guideline_vis.csv",df.vis)
   visRelEmoServer("visRelEmo1","data/guideline_vis.csv",df.vis)
+  
+  # Disable all tabs except Disclaimer initially
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='input_tab']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='tvrag']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='tdataset']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='tblij']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='tverd']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='tpijn']")
+  shinyjs::disable(selector = "#main\\.navbar li a[data-value='temo']")
 }
+
 shinyApp(ui, server)
